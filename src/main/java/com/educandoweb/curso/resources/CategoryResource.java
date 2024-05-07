e8892543532331ce3a9bcc2a855886c400ea58af
rafael_nunes <rafa.nuns17@gmail.com>
2024-05-07 11:36:25 -0300

    Refatorando codigo de Category retornando mensagem de 201 created

--- src/main/java/com/educandoweb/curso/resources/CategoryResource.java
+++ src/main/java/com/educandoweb/curso/resources/CategoryResource.java
@@ -1,5 +1,6 @@
 package com.educandoweb.curso.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.curso.entities.Category;
 import com.educandoweb.curso.services.CategoryService;
@@ -36,6 +38,7 @@ public class CategoryResource {
 	@PostMapping
 	public ResponseEntity<Category> insert(@RequestBody Category obj) {
 		obj = service.insert(obj);
-		return ResponseEntity.ok().body(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
