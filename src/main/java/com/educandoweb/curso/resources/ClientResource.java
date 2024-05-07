481f7265fea0e6f0412678b3fab6f8addfb0b85d
rafael_nunes <rafa.nuns17@gmail.com>
2024-05-07 11:39:33 -0300

    Refatorando codigo de Client retornando mensagem de 201 created

--- src/main/java/com/educandoweb/curso/resources/ClientResource.java
+++ src/main/java/com/educandoweb/curso/resources/ClientResource.java
@@ -1,5 +1,6 @@
 package com.educandoweb.curso.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.curso.entities.Client;
 import com.educandoweb.curso.services.ClientService;
@@ -36,6 +38,7 @@ public class ClientResource {
 	@PostMapping
 	public ResponseEntity<Client> insert(@RequestBody Client obj) {
 		obj = service.insert(obj);
-		return ResponseEntity.ok().body(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
