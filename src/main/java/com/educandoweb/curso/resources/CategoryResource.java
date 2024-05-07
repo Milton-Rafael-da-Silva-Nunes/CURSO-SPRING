afe3015f2dd2ab4528fc6064efa9446088fbac3d
rafael_nunes <rafa.nuns17@gmail.com>
2024-05-07 11:10:26 -0300

    Adicionando metodo POST de inserção de USER, CLIENT e CATEGORY

--- src/main/java/com/educandoweb/curso/resources/CategoryResource.java
+++ src/main/java/com/educandoweb/curso/resources/CategoryResource.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +30,12 @@ public class CategoryResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Category> findById(@PathVariable Long id) {
 		Category obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Category> insert(@RequestBody Category obj) {
+		obj = service.insert(obj);
 		return ResponseEntity.ok().body(obj);
 	}
 }
