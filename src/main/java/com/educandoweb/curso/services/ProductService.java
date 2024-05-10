b8540edfa651fa918f4dfaeb069c782239fa689e
rafael_nunes <rafa.nuns17@gmail.com>
2024-05-10 12:48:41 -0300

    Adicionando exceção personalizada para Deletar produto

--- src/main/java/com/educandoweb/curso/services/ProductService.java
+++ src/main/java/com/educandoweb/curso/services/ProductService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.curso.entities.Product;
 import com.educandoweb.curso.repositories.ProductRepository;
+import com.educandoweb.curso.services.exceptions.DatabaseException;
 import com.educandoweb.curso.services.exceptions.ResourceNotFoundException;
 
 
@@ -31,7 +34,13 @@ public class ProductService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public Product update(Long id, Product obj) {
